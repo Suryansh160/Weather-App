056a92147d3e6c16c7c9292c16dba4ac2c2c2314
suryansh <suryanshgupta160@gmail.com>
2025-03-19 21:19:34 +0530

    grid completed

--- lib/weatherScreen.dart
+++ lib/weatherScreen.dart
@@ -38,7 +38,7 @@ class _WeatherscreenState extends State<Weatherscreen> {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: Text(
-          city.toUpperCase(),
+          city,
           style: TextStyle(color: Colors.white),
         ),
         actions: [
@@ -74,12 +74,12 @@ class _WeatherscreenState extends State<Weatherscreen> {
           SingleChildScrollView(
             child: Column(
               children: [
-                Padding(padding: EdgeInsets.only(top: screenHeight * 0.15)),
+                Padding(padding: EdgeInsets.only(top: screenHeight * 0.17)),
                 Center(
                   child: Text(
                     "${fullResponse != null ? fullResponse![0]['main']['temp'].toInt().toString() : 'N/A'}°",
                     style: TextStyle(
-                        fontSize: 55,
+                        fontSize: screenHeight * 0.1,
                         color: Colors.white,
                         fontWeight: FontWeight.w500),
                   ),
@@ -88,8 +88,140 @@ class _WeatherscreenState extends State<Weatherscreen> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Text(
-                        "${fullResponse != null ? fullResponse![0]['weather'][0]['main'] : 'N/A'}"),
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.w400),
+                        "${fullResponse != null ? fullResponse![0]['weather'][0]['main'] : 'N/A'} ${fullResponse != null ? fullResponse![0]['main']['temp_min'] : 'N/A'}° / ${fullResponse != null ? fullResponse![0]['main']['temp_max'] : 'N/A'}° Humidity ${fullResponse != null ? fullResponse![0]['main']['humidity'] : 'N/A'}"),
                   ],
+                ),
+                SizedBox(
+                  height: screenHeight * 0.00005,
+                ),
+                Container(
+                  height: screenHeight * 0.4,
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: GridView.count(
+                    childAspectRatio: 1.3,
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 8,
+                    mainAxisSpacing: 10,
+                    children: [
+                      Container(
+                        // height: screenHeight * 0.08,
+                        decoration: BoxDecoration(
+                          color: Colors.white
+                              .withOpacity(0.3), // Transparent background
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(Icons.water, color: Colors.white),
+                            Text('Sea Level',
+                                style: TextStyle(color: Colors.white)),
+                            Text(
+                                '${fullResponse != null ? fullResponse![0]['main']['sea_level'] : 'N/A'}',
+                                style: TextStyle(color: Colors.white)),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white
+                              .withOpacity(0.3), // Transparent background
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(Icons.thermostat, color: Colors.white),
+                            Text('Feels Like',
+                                style: TextStyle(color: Colors.white)),
+                            Text(
+                                '${fullResponse != null ? fullResponse![0]['main']['feels_like'] : 'N/A'} °',
+                                style: TextStyle(color: Colors.white)),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white
+                              .withOpacity(0.3), // Transparent background
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(Icons.water_drop_outlined,
+                                color: Colors.white),
+                            Text('Humidity',
+                                style: TextStyle(color: Colors.white)),
+                            Text(
+                                style: TextStyle(color: Colors.white),
+                                '${fullResponse != null ? fullResponse![0]['main']['humidity'] : 'N/A'}'),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white
+                              .withOpacity(0.3), // Transparent background
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(Icons.air, color: Colors.white),
+                            Text('WNW wind',
+                                style: TextStyle(color: Colors.white)),
+                            Text(
+                                '${fullResponse != null ? fullResponse![0]['wind']['speed'] : 'N/A'} mph',
+                                style: TextStyle(color: Colors.white)),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white
+                              .withOpacity(0.3), // Transparent background
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(Icons.compress, color: Colors.white),
+                            Text('Air pressure',
+                                style: TextStyle(color: Colors.white)),
+                            Text(
+                                style: TextStyle(color: Colors.white),
+                                '${fullResponse != null ? fullResponse![0]['main']['pressure'] : 'N/A'} hPa'),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white
+                              .withOpacity(0.3), // Transparent background
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              Icons.visibility_outlined,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              'Visibility',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            Text(
+                                '${fullResponse != null ? fullResponse![0]['visibility'] : 'N/A'}',
+                                style: TextStyle(color: Colors.white)),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 )
               ],
             ),
